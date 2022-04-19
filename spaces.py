3ea64cdfda54f3d770fec5ed566d23979daff459
mohadeseh <m.ghafoori78@gmail.com>
2022-04-19 19:01:31 +0430

    BGR to RGB

--- spaces.py
+++ spaces.py
@@ -1,3 +1,4 @@
+from colorsys import rgb_to_hls
 import cv2 as cv
 img=cv.imread("G:\learn opencv/DIP.jpg") #by default opencv will read image in BGR format 
 cv.imshow("original image", img)
@@ -10,4 +11,7 @@ cv.imshow("hsv space",hsv_img)
 #BGR to LAB
 lab_img=cv.cvtColor(img,cv.COLOR_BGR2LAB)
 cv.imshow("LAB space",lab_img)
+#BGR to RGB
+rgb_img=cv.cvtColor(img,cv.COLOR_BGR2RGB)
+cv.imshow("RGB space",rgb_img)
 cv.waitKey(0)
