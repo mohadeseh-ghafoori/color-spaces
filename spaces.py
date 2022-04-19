5a188aab939534a5f272004a769cd3993cdbe0dc
mohadeseh <m.ghafoori78@gmail.com>
2022-04-19 18:50:57 +0430

    BGR to HSV

--- spaces.py
+++ spaces.py
@@ -1,7 +1,10 @@
 import cv2 as cv
 img=cv.imread("G:\learn opencv/DIP.jpg") #by default opencv will read image in BGR format 
 cv.imshow("original image", img)
-#gray scale
+#BGR to gray
 gray_img=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow("gray scale image", gray_img)
+#BGR to HSV
+hsv_img=cv.cvtColor(img, cv.COLOR_BGR2HSV)
+cv.imshow("hsv space",hsv_img)
 cv.waitKey(0)
